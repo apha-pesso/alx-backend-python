258ce29f98a46740223dace900f600b3899dba67
apha-pesso <mutiuayo99@gmail.com>
2023-03-30 13:02:47 +0100

    Fix pycodestyle and documentation

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+"""Function"""
 from typing import Callable
 
-def make_multiplier(multiplier: float) -> Callable[[float, float]:
-        def inner(multiplier: float) -> float:
-            return (multiplier * multiplier)
-        return inner
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return function"""
+    def inner(multiplier: float) -> float:
+        """Return multiplier"""
+        return (multiplier * multiplier)
+    return inner
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""Annotation"""
 from typing import Iterable, List, Sequence, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Annotation"""
     return [(i, len(i)) for i in lst]
