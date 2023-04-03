a1192a6f48b496a382fb449e9cddc0e7f2d90ff9
apha-pesso <mutiuayo99@gmail.com>
2023-04-03 18:33:30 +0100

    Async exercise

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Multiple coroutines"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    '''Multiple coroutine'''
+    """tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
+    value = []
+    for i in range(n):
+        a = await wait_random(max_delay)
+        value.append(a)
+    return value
+    """
+    values = []
+    for _ in range(n):
+        values.append(wait_random(max_delay))
+    res = await asyncio.gather(*values)
+    return sorted(res)
