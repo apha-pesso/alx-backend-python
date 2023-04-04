5d3580ecb804cc648ff4030fb9592eb467eb6e14
apha-pesso <mutiuayo99@gmail.com>
2023-04-04 12:05:17 +0100

    Change Return annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''Async comprehension'''
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Async comprehension"""
     """
     list_n = []
