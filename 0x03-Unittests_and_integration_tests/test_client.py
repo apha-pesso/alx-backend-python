1f8ce2163483e16e3e01b0d328c2085b7b4e3439
apha-pesso <mutiuayo99@gmail.com>
2023-04-30 23:40:27 +0100

    Test org

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''Client test'''
+from parameterized import parameterized
+from client import GithubOrgClient
+from unittest.mock import patch
+import unittest
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''TEsting GithubOrg'''
+    @parameterized.expand([
+        ('google', {"payload": True}),
+        ('abc', {"payload": True})])
+    @patch('client.get_json')
+    def test_org(self, url_ext, expected, get_json_patch):
+        test_org = GithubOrgClient(url_ext)
+        url = f"https://api.github.com/orgs/{url_ext}"
+
+        get_json_patch.return_value = expected
+
+        self.assertEqual(test_org.org, expected)
+        get_json_patch.assert_called_once_with(url)
+
+        # expected = requests.get(url).json()
+        # self.assertEqual(test_org.org(), expected)
